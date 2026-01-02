b5af15d038c72043ce17d841f59f97178bfd40f8
Caleb Husovich <calebmhusovich@gmail.com>
2026-01-02 09:06:42 -0500

    hardcoded system type for pkgs

--- configuration.nix
+++ configuration.nix
@@ -19,8 +19,8 @@
  
  # Camera packages
   environment.systemPackages = [
-    nixos-raspberrypi.packages.${pkgs.system}.libcamera
-    nixos-raspberrypi.packages.${pkgs.system}.rpicam-apps
+    nixos-raspberrypi.packages.aarch-64.libcamera
+    nixos-raspberrypi.packages.aarch-64.rpicam-apps
   ];
  
   # Network Configuration
--- justfile
+++ justfile
@@ -1,2 +1,5 @@
 install:
   nix run nixpkgs#nixos-anywhere -- --flake .#artemis --disk-config ./disk-config.nix --reboot --generate-hardware-config nixos-generate-config /hardware-configuration.nix root@artemis
+
+test:
+  nix run github:nix-community/nixos-anywhere -- --flake .#artemis --vm-test
